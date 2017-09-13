35f80f3930aa93ee71be5d1fa9a9937ba36a23d3
Kefeng Deng <kefeng.deng@fairfaxmedia.co.nz>
2017-09-13 23:38:55 +1200

    init commit

---
+++ jboss-fuse-full/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DOCKER_IMAGE_NAME=ffxnz/jboss-fuse-full
+DOCKER_IMAGE_VERSION=latest
+
+docker rmi --force=true ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}
+docker build --force-rm=true --rm=true -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} .
+echo =========================================================================
+echo Docker image is ready.  Try it out by running:
+echo     docker run --rm -ti -P ${DOCKER_IMAGE_NAME}
+echo =========================================================================
